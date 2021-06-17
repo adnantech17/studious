54d05a31d837473a94c2e12f16408d488cbb7c4e
Mohidul Haque Mridul <mridul.haque.mh@gmail.com>
2021-06-17 18:49:22 +0600

    Add package-lock.json in git

--- Navigation/SignedInNav.js
+++ Navigation/SignedInNav.js
@@ -14,9 +14,9 @@ const SignedInNav = () => {
                 keyboardHidesTabBar: true
             }}
         >
-            <Tab.Screen name="Events" component={EventsNav} />
-            <Tab.Screen name="Todos" component={TodosNav} />
             <Tab.Screen name="Materials" component={MaterialsNav} />
+            <Tab.Screen name="Todos" component={TodosNav} />
+            <Tab.Screen name="Events" component={EventsNav} />
             <Tab.Screen name="Profile" component={ProfilesNav} />
         </Tab.Navigator>
     );
